1e4397e52a8aed23c85100aee43f7cf6ea81a544
LupoNetn <luponet07@gmail.com>
2024-12-08 09:43:38 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const cityName = document.getElementById('city-query');
+const weatherIcon = document.querySelector('.weather-icon');
+const apiKey = 'aaf8fa490f8aada395cf0c4f70360b57';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    if (response.status == 404) {
+        document.querySelector('.error').style.display = 'block';
+        document.querySelector('.weather').style.display = 'none';
+        setTimeout(() => {
+            document.querySelector('.error').style.display = 'none'; 
+        }, 2000);
+    } else {
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°c';
+    document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
+    document.querySelector('.wind').innerHTML = data.wind.speed + 'km/hr'
+
+    if (data.weather[0].main == 'Clouds') {
+        weatherIcon.src = 'images/clouds.png'
+    } else if (data.weather[0].main == 'Rain') {
+        weatherIcon.src = 'images/rain.png'
+    } else if (data.weather[0].main == 'Clear') {
+        weatherIcon.src = 'images/clear.png'
+    } else if (data.weather[0].main == 'Drizzle') {
+        weatherIcon.src = 'images/drizzle.png'
+    } else if (data.weather[0].main == 'Mist') {
+        weatherIcon.src = 'images/mist.png'
+    } else if (data.weather[0].main == 'Snow') {
+        weatherIcon.src = 'images/snow.png'
+    }
+
+    document.querySelector('.weather').style.display = 'block'
+    cityName.value = ''
+
+    }
+
+}
+
+document.getElementById('btn').addEventListener('click', () => {
+    checkWeather(cityName.value);
+})
+
